a2706088d95a39387879b0c28eb47ca52696c85c
joerald <ngozika1105@gmail.com>
2024-05-01 13:38:50 +0100

    set up expressjs postgre ans sequelize ORM

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 const express = require('express' );
 const app = express();
+
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Create a catch-all route for testing the installation.
+app.get('*', (req, res) => {
+    res.status(200).send('welcome')
+});
+
 const port = 8080;
-app.get('/', (req, res) => {
-    res.send('welcome')
-})
-app.listen(port, () => { `Example app listening on ${port}`})
+app.listen(port, () => { `ecommerce app listening on ${port}`})b
